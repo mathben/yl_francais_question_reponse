326d6ef8a6debbf2835f134adacb22bc55afada4
Mathieu Benoit <mathben@technolibre.ca>
2022-08-11 05:41:04 -0400

    [ADD] valider argument, ajout debug, colorer les mots spéciaux

--- questionnaire.py
+++ questionnaire.py
@@ -6,6 +6,7 @@ import logging
 import csv
 from colorama import Fore, Style
 import signal
+import os
 import time
 
 EXPECTED_HEADERS_CSV = "Question,Réponse,Choix A,Choix B,Choix C,Choix D,Explication"
@@ -16,15 +17,30 @@ def main():
     parser = argparse.ArgumentParser(description="Generate your vaccination QR code.")
     parser.add_argument("-d", "--database", type=str, help="Your database filename path",
                         default="./database.csv")
-    parser.add_argument("--response_char", type=str, help="Your character to start the answer, 0, 1 or a (A)",
+    parser.add_argument("--response_char", type=str,
+                        help="Le caractère qui représente la réponse. Suggestion : 0, 1, a. Elle est insensible (minuscule ou majuscule).",
                         default="a")
     parser.add_argument("-q", "--question", type=int, help="Question index to begin, start at 1 by default", default=1)
     parser.add_argument("--mot_souligne", type=str,
-                        help="1 character, when detect 2 times this char, underline words when show question/answer",
+                        help="Ensemble de caractère à détecter, lorqu'il apparait 2 fois, il met le(s) mot(s) en couleur dans le choix de réponse.",
                         default="X")
     # parser.add_argument("-v", "--verbose", action="store_true",
     #                     help="verbose output")
+    parser.add_argument("--debug", action="store_true",
+                        help="Affiche toutes les questions et réponses en ignorant la saisie de données des réponses.")
+    parser.add_argument("--debug_delay", type=int,
+                        help="Lorsque debug est activé, délai pour faire afficher la réponse en seconde.", default=0)
     args = parser.parse_args()
+
+    if len(args.response_char) != 1:
+        raise Exception(
+            f"Le paramètre --response_char doit contenir seulement 1 caractère. Présentement '{args.response_char}'.")
+
+    if args.question <= 0:
+        raise Exception(f"Le numéro de question doit être supérieur ou égale à 1, valeur actuel : '{args.question}'")
+
+    if not os.path.exists(args.database):
+        raise Exception(f"Le fichier de base de données n'existe pas, path : '{args.database}'")
 
     qp = QuestionParser(args)
 
@@ -76,20 +92,41 @@ class QuestionParser:
                     print(f'{Fore.GREEN}Question {self._question_number}{Style.RESET_ALL}')
                     print(row[0])
                     print()
+                    self.print_response(row, 2)
                     self.print_response(row, 3)
                     self.print_response(row, 4)
                     self.print_response(row, 5)
-                    self.print_response(row, 6)
-                    # time.sleep(3)
                     print()
-            print(f'Questionnaire complété!')
+                    if self.parser.debug:
+                        if self.parser.debug_delay:
+                            time.sleep(self.parser.debug_delay)
+                        print("réponse")
+                        print(row[1])
+                        print()
+                        print(f"{Fore.BLUE}{row[6]}{Style.RESET_ALL}")
+                    else:
+                        print()
+                    print()
+            print(f'{Fore.RED}Questionnaire complété!{Style.RESET_ALL}')
             self.print_result()
 
     def print_response(self, row, no):
         # mot_souligne
-        response_char = no - 3
+        response_char = no - 2
         transform_response_char = chr(ord(self.parser.response_char) + response_char).upper()
-        print(f'\t{transform_response_char} - {row[no]}')
+        sentence = row[no]
+        # Detect special word
+        if sentence.count(self.parser.mot_souligne) >= 2:
+            lst_sentence = sentence.split(self.parser.mot_souligne)
+            new_sentence = ""
+            for index, s in enumerate(lst_sentence):
+                if index % 2:
+                    # Pour tous les mots pairs, on les colore
+                    new_sentence += f"{Fore.MAGENTA}{s}{Style.RESET_ALL}"
+                else:
+                    new_sentence += s
+            sentence = new_sentence
+        print(f'\t{transform_response_char} - {sentence}')
 
 
 if __name__ == "__main__":
